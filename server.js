02bc3d5533df058face4ea5935bc7901ea5d2487
Joaquin Huerta <joaco.011@gmail.com>
2022-11-16 22:53:58 -0300

    wokring rapper api

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express();
+const PORT = 8000;
+
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthName': 'Real name asdnlkasjdna',
+        'birthlocation': 'London, England'
+    },
+    'chance the rapper':{
+        'age': 10,
+        'birthName': 'Real name chance',
+        'birthlocation': 'Chicago, England'
+    },
+    'unknown':{
+        'age': 0,
+        'birthName': 'unknown',
+        'birthlocation': 'unknown'
+    }
+}
+
+
+app.get('/', (request, response)=>{ // it is a network request
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+    const rapperName = request.params.name.toLowerCase();
+    if(rappers[rapperName]){
+        response.json(rappers[rapperName])
+    }else{
+        response.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT , ()=>{
+    console.log(`the server is now running in port ${PORT}! Betta Go Catch it!`)
+})
